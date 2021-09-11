fbd7299618e33f31d83a56f86eadccf6ba1317bc
Adiel <adielp16@gmail.com>
2021-09-11 17:34:47 -0300

    amount by seller

--- backend/src/main/java/dev/adiel/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/dev/adiel/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package dev.adiel.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import dev.adiel.dsvendas.dtos.SaleSumDTO;
 import dev.adiel.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("SELECT new dev.adiel.dsvendas.dtos.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
 }
--- backend/src/main/java/dev/adiel/dsvendas/services/SaleService.java
+++ backend/src/main/java/dev/adiel/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package dev.adiel.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.adiel.dsvendas.dtos.SaleDTO;
+import dev.adiel.dsvendas.dtos.SaleSumDTO;
 import dev.adiel.dsvendas.entities.Sale;
 import dev.adiel.dsvendas.repositories.SaleRepository;
 import dev.adiel.dsvendas.repositories.SellerRepository;
@@ -25,5 +28,10 @@ public class SaleService {
 		sellerRepository.findAll(); // armazenar em memoria pra evitar muitas consultas ao DB
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(sale -> new SaleDTO(sale));
+	}
+	
+	@Transactional(readOnly= true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
 	}
 }
