1522258c24e3ac455d973d7dd909b77be24dad13
Adiel <adielp16@gmail.com>
2021-09-11 13:21:51 -0300

    projet created

---
+++ backend/src/test/java/dev/adiel/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adiel.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
