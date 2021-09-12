341108fc729b7ed8d5569f9d6ebfb585dc3ca937
Adiel <adielp16@gmail.com>
2021-09-12 13:02:58 -0300

    pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,9 +1,11 @@
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { api } from "utils/requests";
 
 const DataTable = () => {
+	const [activePage, setActivePage] = useState<number>(0);
 	const [page, setPage] = useState<SalePage>({
 		first: true,
 		last: true,
@@ -13,41 +15,48 @@ const DataTable = () => {
 	});
 
 	useEffect(() => {
-		api.get('/sales?page=1&size=10&sort=date,desc')
+		api.get(`/sales?page=${activePage}&size=20&sort=date,desc`)
 			.then(result => {
 				setPage(result.data);
 			}).catch(err => {
 				console.error(err);
 			})
-	}, []);
+	}, [activePage]);
+
+	const changePage = (index: number) => {
+		setActivePage(index);
+	}
 
   return (
-	<div className="table-responsive">
-	  <table className="table table-striped table-sm">
-		  <thead>
-			  <tr>
-				  <th>Data</th>
-				  <th>Vendedor</th>
-				  <th>Clientes visitados</th>
-				  <th>Negócios fechados</th>
-				  <th>Valor</th>
-			  </tr>
-		  </thead>
-		  <tbody>
-				{
-					page.content?.map(item => (
-						<tr key={item.id}>
-							<td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
-							<td>{item.seller?.name}</td>
-							<td>{item.visited}</td>
-							<td>{item.deals}</td>
-							<td>{item.amount.toFixed(2)}</td>
+		<>
+			<Pagination page={page} onPageChange={changePage}/>
+			<div className="table-responsive">
+				<table className="table table-striped table-sm">
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th>Vendedor</th>
+							<th>Clientes visitados</th>
+							<th>Negócios fechados</th>
+							<th>Valor</th>
 						</tr>
-					))
-				}
-		  </tbody>
-	  </table>
-	</div>
+					</thead>
+					<tbody>
+						{
+							page.content?.map(item => (
+								<tr key={item.id}>
+									<td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
+									<td>{item.seller?.name}</td>
+									<td>{item.visited}</td>
+									<td>{item.deals}</td>
+									<td>{item.amount.toFixed(2)}</td>
+								</tr>
+							))
+						}
+					</tbody>
+				</table>
+			</div>
+		</>
   );
 }
 
