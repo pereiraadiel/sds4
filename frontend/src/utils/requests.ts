aad39ffb7911fa9150fc240147f86a7f0c3c8dbc
Adiel <adielp16@gmail.com>
2021-09-12 13:08:04 -0300

    env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 export const api = axios.create({
   baseURL: BASE_URL
